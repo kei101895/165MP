81c4cba9bdea9cdb7147922eec535cd530345679
Kei Shirabe <pokemaniackei@gmail.com>
2016-12-19 21:47:25 +0800

    changed connect

--- connect.php
+++ connect.php
@@ -2,10 +2,12 @@
 
 $connect_error = 'Could not connect!';
 
-if(!$conn = pg_connect("host=localhost port=5432 dbname=shirabe user=postgres password=securepassword")){
+if(!$conn = pg_connect("host=ec2-184-72-249-88.compute-1.amazonaws.com port=5432 dbname=d6dlfr0f6cge6k user=bfcaagrcffgwsz password=3131f4349f012b19465871b1eb0160bb039ec8f6c7d3f51faac3a2a398e0feeb")){
 	die($connect_error);
 } else{
 	
 }
 
 ?>
+
+
