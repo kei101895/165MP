a89a3375e8683c0181f66ff3c13acd600a7070c6
Kei Shirabe <pokemaniackei@gmail.com>
2016-12-19 21:48:34 +0800

    oh no

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect_error = 'Could not connect!';
+$connect_error = 'Could not connect! oh no';
 
 if(!$conn = pg_connect("host=ec2-184-72-249-88.compute-1.amazonaws.com port=5432 dbname=d6dlfr0f6cge6k user=bfcaagrcffgwsz password=3131f4349f012b19465871b1eb0160bb039ec8f6c7d3f51faac3a2a398e0feeb")){
 	die($connect_error);
